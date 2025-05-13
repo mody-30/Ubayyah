97da852993d320c047c6dc0bff8071a443232d6e
mody-30 <meshal1amri@gamil.com>
2025-05-13 05:32:18 +0300

    Fix conflict between app and pages directory

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// app/page.tsx
+
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      {/* Add more content as needed */}
+    </div>
+  );
+};
+
+export default HomePage;
